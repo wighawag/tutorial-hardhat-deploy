5aef28f90cecc9a79b669e29990d5d100543400d
Wighawag <wighawag@gmail.com>
2021-03-23 11:29:28 +0000

    add deps

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,13 @@
 import {HardhatUserConfig} from 'hardhat/types';
+import 'hardhat-deploy';
+import 'hardhat-deploy-ethers';
+
 const config: HardhatUserConfig = {
   solidity: {
     version: '0.8.2',
-  }
+  },
+  namedAccounts: {
+    deployer: 0,
+  },
 };
 export default config;
