0f57bd7ad2d50dbad5a115c5f587ee52d013810a
Wighawag <wighawag@gmail.com>
2021-03-23 13:37:25 +0000

    deployments to rinkeby

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,17 @@
 import {HardhatUserConfig} from 'hardhat/types';
 import 'hardhat-deploy';
 import 'hardhat-deploy-ethers';
+import {node_url, accounts} from './utils/network';
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.2',
+    version: '0.7.6',
+  },
+  networks: {
+    rinkeby: {
+      url: node_url('rinkeby'),
+      accounts: accounts('rinkeby'),
+    },
   },
   namedAccounts: {
     deployer: 0,
--- src/Token.sol
+++ src/Token.sol
@@ -2,7 +2,7 @@
 // The line above is recommended and let you define the license of your contract
 // Solidity files have to start with this pragma.
 // It will be used by the Solidity compiler to validate its version.
-pragma solidity ^0.8.2;
+pragma solidity ^0.7.6;
 
 
 // This is the main building block for smart contracts.
