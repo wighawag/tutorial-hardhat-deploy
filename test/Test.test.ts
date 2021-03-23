e2ce66dd474053fe867ecadbd1214820314acae0
Wighawag <wighawag@gmail.com>
2021-03-23 12:02:00 +0000

    fix

--- test/Test.test.ts
+++ test/Test.test.ts
@@ -1,10 +1,10 @@
 import {expect} from "./chai-setup";
 
-import {ethers, deployments, getUnnamedAccounts, getNamedAccounts} from 'hardhat';
+import {ethers, deployments, getNamedAccounts} from 'hardhat';
 
 describe("Token contract", function() {
   it("Deployment should assign the total supply of tokens to the owner", async function() {
-    await deployments.fixture(["Token"])
+    await deployments.fixture(["Token"]);
     const {tokenOwner} = await getNamedAccounts();
     const Token = await ethers.getContract("Token");
     const ownerBalance = await Token.balanceOf(tokenOwner);
